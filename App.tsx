af145cd7a468a5b3d3d3c506682187316513ecbc
oscarpiresjunior <68670360+oscarpiresjunior@users.noreply.github.com>
2025-10-29 07:54:00 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -217,7 +217,7 @@ const App: React.FC = () => {
             console.error("Error updating profile", profileError);
         } else {
             setUser({ ...user, ...updatedProfile });
-            fetchBooks();
+            await fetchBooks();
         }
     };
 
